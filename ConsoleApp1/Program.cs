17b160507e4e33d0c048875e7663459620b5729f
Kamranbb <kamrannb@code.edu.az>
2024-02-13 16:49:23 +0400

    feat:algoritm-tasks

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region task1
+//int n = 8;
+// if (n%2 ==0)
+//{ Console.WriteLine("eded cutdur"); 
+//}
+// else
+//{
+//    Console.WriteLine("eded tekdir");
+//}
+#endregion
+
+
+#region task2
+
+//int n = 27;
+// if (n%3==0 && n%7==0)
+//{
+//    Console.WriteLine("qaliqsiz bolunur");
+
+//}
+// else
+//{
+//    Console.WriteLine("qaliqsiz bolunmur");
+//}
+
+
+#endregion
+
+#region task3
+//int n, m;
+//n = 20;
+//m = 11;
+//if (n%2==0 && m%2==0)
+//{
+//    Console.WriteLine(n+m);
+//}
+//else
+//{
+//    Console.WriteLine(n-m);
+//}
+#endregion
+
+#region task4
+//int temp = 25;
+//    if ( temp > 15 )
+
+//{
+//    Console.WriteLine("hava istdir");
+//}
+//    else
+
+//    { Console.WriteLine("hava soyuqdur"); 
+//}
+
+#endregion
+
